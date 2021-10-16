300c9f31f645f38c693d42d227414d288c2d731b
Christian Raoulis <craoulis@googlemail.com>
2021-10-16 15:41:56 +0200

    add Tabs widgets to all DefaultTabContainer classes

--- src/main/java/de/blutmondgilde/otherlivingbeings/handler/InventoryTabHandler.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/handler/InventoryTabHandler.java
@@ -1,9 +1,12 @@
 package de.blutmondgilde.otherlivingbeings.handler;
 
 import de.blutmondgilde.otherlivingbeings.api.gui.inventory.tabs.AbstractInventoryTab;
+import de.blutmondgilde.otherlivingbeings.api.gui.inventory.tabs.DefaultTabContainerScreen;
 import de.blutmondgilde.otherlivingbeings.api.gui.inventory.tabs.InventoryTabSwitcherWidget;
 import de.blutmondgilde.otherlivingbeings.api.gui.inventory.tabs.TabPosition;
 import de.blutmondgilde.otherlivingbeings.registry.InventoryTabRegistry;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -16,12 +19,11 @@ public class InventoryTabHandler {
     }
 
     public static void onPlayerInventory(GuiScreenEvent.InitGuiEvent e) {
-        if (!(e.getGui() instanceof InventoryScreen inventoryScreen)) return;
-        int screenWidth = inventoryScreen.width - inventoryScreen.getGuiLeft();
-        int screenHeight = inventoryScreen.height - inventoryScreen.getGuiTop();
+        if (!isTabScreen(e.getGui())) return;
+        AbstractContainerScreen screen = (AbstractContainerScreen) e.getGui();
 
         final Map<Integer, AbstractInventoryTab> tabRegistryValues = InventoryTabRegistry.getIdTabMap();
-        InventoryTabSwitcherWidget tabSwitcher = new InventoryTabSwitcherWidget(inventoryScreen, (int) Math.round(Math.ceil(tabRegistryValues.size() / 12F)));
+        InventoryTabSwitcherWidget tabSwitcher = new InventoryTabSwitcherWidget(screen, (int) Math.round(Math.ceil(tabRegistryValues.size() / 12F)));
 
         for (Map.Entry<Integer, AbstractInventoryTab> entry : tabRegistryValues.entrySet()) {
             AbstractInventoryTab widget = entry.getValue();
@@ -36,33 +38,33 @@ public class InventoryTabHandler {
 
             switch (posIndex) {
                 case 1 -> {
-                    widget.x = inventoryScreen.getGuiLeft();
-                    widget.y = inventoryScreen.getGuiTop() - widget.getHeight() + yOffset;
+                    widget.x = screen.getGuiLeft();
+                    widget.y = screen.getGuiTop() - widget.getHeight() + yOffset;
                     widget.setPosition(TabPosition.LEFT_TOP);
                 }
                 case 2, 3, 4, 5 -> {
-                    widget.x = inventoryScreen.getGuiLeft() + widget.getWidth() * (posIndex - 1) + (xOffset * posIndex - 1);
-                    widget.y = inventoryScreen.getGuiTop() - widget.getHeight() + yOffset;
+                    widget.x = screen.getGuiLeft() + widget.getWidth() * (posIndex - 1) + (xOffset * posIndex - 1);
+                    widget.y = screen.getGuiTop() - widget.getHeight() + yOffset;
                     widget.setPosition(TabPosition.TOP);
                 }
                 case 6 -> {
-                    widget.x = inventoryScreen.getGuiLeft() + widget.getWidth() * (posIndex - 1) + (xOffset * posIndex - 1);
-                    widget.y = inventoryScreen.getGuiTop() - widget.getHeight() + yOffset;
+                    widget.x = screen.getGuiLeft() + widget.getWidth() * (posIndex - 1) + (xOffset * posIndex - 1);
+                    widget.y = screen.getGuiTop() - widget.getHeight() + yOffset;
                     widget.setPosition(TabPosition.RIGHT_TOP);
                 }
                 case 7 -> {
-                    widget.x = inventoryScreen.getGuiLeft();
-                    widget.y = inventoryScreen.getGuiTop() + inventoryScreen.imageWidth - yOffset;
+                    widget.x = screen.getGuiLeft();
+                    widget.y = screen.getGuiTop() + screen.imageWidth - yOffset;
                     widget.setPosition(TabPosition.LEFT_BOT);
                 }
                 case 8, 9, 10, 11 -> {
-                    widget.x = inventoryScreen.getGuiLeft() + widget.getWidth() * (posIndex - 7) + (xOffset * posIndex - 1);
-                    widget.y = inventoryScreen.getGuiTop() + inventoryScreen.imageWidth - yOffset;
+                    widget.x = screen.getGuiLeft() + widget.getWidth() * (posIndex - 7) + (xOffset * posIndex - 1);
+                    widget.y = screen.getGuiTop() + screen.imageWidth - yOffset;
                     widget.setPosition(TabPosition.BOT);
                 }
                 case 12 -> {
-                    widget.x = inventoryScreen.getGuiLeft() + widget.getWidth() * (posIndex - 7) + (xOffset * posIndex - 1);
-                    widget.y = inventoryScreen.getGuiTop() + inventoryScreen.imageWidth - yOffset;
+                    widget.x = screen.getGuiLeft() + widget.getWidth() * (posIndex - 7) + (xOffset * posIndex - 1);
+                    widget.y = screen.getGuiTop() + screen.imageWidth - yOffset;
                     widget.setPosition(TabPosition.RIGHT_BOT);
                 }
             }
@@ -72,5 +74,11 @@ public class InventoryTabHandler {
 
         tabSwitcher.updateTabs();
         e.addWidget(tabSwitcher);
+    }
+
+    private static boolean isTabScreen(Screen screen) {
+        if (screen instanceof InventoryScreen) return true;
+        if (screen instanceof DefaultTabContainerScreen) return true;
+        return false;
     }
 }
