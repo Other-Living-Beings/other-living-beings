f445d693917740393d2b5e9b518cfef182cc099c
Christian Raoulis <craoulis@googlemail.com>
2021-10-18 15:25:22 +0200

    add black fill background to DefaultTabContainerScreen

--- src/main/java/de/blutmondgilde/otherlivingbeings/api/gui/inventory/tabs/DefaultTabContainerScreen.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/api/gui/inventory/tabs/DefaultTabContainerScreen.java
@@ -9,6 +9,8 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
+import java.awt.*;
+
 public class DefaultTabContainerScreen<T extends DefaultTabContainer> extends EffectRenderingInventoryScreen<T> {
     public static final ResourceLocation INVENTORY_LOCATION = new ResourceLocation(OtherLivingBeings.MOD_ID, "textures/gui/inventory.png");
 
@@ -18,6 +20,7 @@ public class DefaultTabContainerScreen<T extends DefaultTabContainer> extends Ef
 
     @Override
     protected void renderBg(PoseStack poseStack, float pPartialTicks, int pMouseX, int pMouseY) {
+        fill(poseStack, 0, 0, this.width, this.height, new Color(14, 15, 17, 204).getRGB());
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, INVENTORY_LOCATION);
