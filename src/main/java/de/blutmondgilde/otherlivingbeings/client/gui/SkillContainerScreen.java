4cb950ca816dd317d83a6f7dea8c97f6590773a3
Christian Raoulis <craoulis@googlemail.com>
2021-10-19 15:56:12 +0200

    Make skills scrollable and scissor widgets out of panel

--- src/main/java/de/blutmondgilde/otherlivingbeings/client/gui/SkillContainerScreen.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/client/gui/SkillContainerScreen.java
@@ -1,5 +1,7 @@
 package de.blutmondgilde.otherlivingbeings.client.gui;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import de.blutmondgilde.otherlivingbeings.OtherLivingBeings;
 import de.blutmondgilde.otherlivingbeings.api.capability.OtherLivingBeingsCapability;
 import de.blutmondgilde.otherlivingbeings.api.gui.inventory.tabs.DefaultTabContainerScreen;
 import de.blutmondgilde.otherlivingbeings.capability.skill.PlayerSkillsImpl;
@@ -11,59 +13,68 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class SkillContainerScreen extends DefaultTabContainerScreen<SkillContainer> {
     private List<SkillWidget> skillWidgets;
     private final TickCountDown tickCountDown;
+    private int yOffset = 0;
 
     public SkillContainerScreen(SkillContainer menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
         this.tickCountDown = new TickCountDown();
         this.tickCountDown.addListener(TickCondition.SKIP_TENTH_TICK, this::update);
 
-        AtomicInteger currentX = new AtomicInteger(this.getGuiLeft() + 6);
-        AtomicInteger currentY = new AtomicInteger(this.getGuiTop() + 6 + Minecraft.getInstance().font.lineHeight);
-        this.skillWidgets = menu.getPlayer().getCapability(OtherLivingBeingsCapability.PLAYER_SKILLS).orElse(new PlayerSkillsImpl()).getSkills()
-                .stream().map(iSkill -> {
+        this.skillWidgets = new ArrayList<>();
+        update();
+    }
 
-                    if (currentY.get() + 20 + 2 > getYSize()) {
-                        currentY.set(this.getGuiTop() + 2 + Minecraft.getInstance().font.lineHeight);
-                        currentX.addAndGet(100);
-                    }
+    @Override
+    public void render(PoseStack pMatrixStack, int pMouseX, int pMouseY, float pPartialTicks) {
+        super.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
+        int y = this.getGuiTop() + 6 + Minecraft.getInstance().font.lineHeight;
 
-                    int y = currentY.getAndAdd(20 + 2);
-                    return new SkillWidget(iSkill, currentX.get(), y);
-                })
-                .collect(Collectors.toList());
+        for (SkillWidget widget : skillWidgets) {
+            //Set Scissor frame
+            double scale = getMinecraft().getWindow().getGuiScale();
+            int scissorStartX = (int) Math.round(getGuiLeft() * scale);
+            int scissorStartY = (int) Math.round((this.height - (getGuiTop() + imageHeight - 95)) * scale);
+            widget.setScissor(scissorStartX, scissorStartY, (int) Math.round(imageWidth * scale), (int) Math.round(56 * scale));
 
-        this.skillWidgets.forEach(this::addRenderableWidget);
+            //adjust position to scroll
+            widget.setY(y + yOffset);
+            y += widget.getHeight() + 2;
+            //check is visible
+            widget.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
+
+        }
     }
 
     public void update() {
         this.skillWidgets.forEach(this::removeWidget);
-        AtomicInteger currentX = new AtomicInteger(this.getGuiLeft() + 6);
-        AtomicInteger currentY = new AtomicInteger(this.getGuiTop() + 6 + Minecraft.getInstance().font.lineHeight);
         this.skillWidgets = getMenu().getPlayer().getCapability(OtherLivingBeingsCapability.PLAYER_SKILLS).orElse(new PlayerSkillsImpl()).getSkills()
-                .stream().map(iSkill -> {
-
-                    if (currentY.get() + 20 + 2 > getYSize()) {
-                        currentY.set(this.getGuiTop() + 2 + Minecraft.getInstance().font.lineHeight);
-                        currentX.addAndGet(100);
-                    }
-
-                    int y = currentY.getAndAdd(20 + 2);
-                    return new SkillWidget(iSkill, currentX.get(), y);
-                })
+                .stream().map(iSkill -> new SkillWidget(iSkill, this.getGuiLeft() + 20, 0))
                 .collect(Collectors.toList());
 
-        this.skillWidgets.forEach(this::addRenderableWidget);
+        this.skillWidgets.forEach(this::addWidget);
     }
 
     @Override
     protected void containerTick() {
         this.tickCountDown.tick();
+    }
+
+    @Override
+    public boolean mouseScrolled(double pMouseX, double pMouseY, double pDelta) {
+        this.yOffset += pDelta * 2;
+        return super.mouseScrolled(pMouseX, pMouseY, pDelta);
+    }
+
+    @Override
+    public boolean mouseClicked(double pMouseX, double pMouseY, int pButton) {
+        OtherLivingBeings.getLogger().info("MouseX: {}, MouseY: {}", pMouseY, pMouseY);
+        return super.mouseClicked(pMouseX, pMouseY, pButton);
     }
 }
--- src/main/java/de/blutmondgilde/otherlivingbeings/client/gui/widget/SkillWidget.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/client/gui/widget/SkillWidget.java
@@ -30,6 +30,10 @@ public class SkillWidget implements GuiEventListener, Widget, NarratableEntry {
     @Getter
     private int x, y, width, height;
     private final Font font;
+    @Setter
+    @Getter
+    private boolean isVisible = true;
+    private int scissorX = 0, scissorWidth = 9000, scissorY = 0, scissorHeight = 9000;
 
     public SkillWidget(ISkill skill, int x, int y) {
         this.skill = skill;
@@ -44,6 +48,8 @@ public class SkillWidget implements GuiEventListener, Widget, NarratableEntry {
 
     @Override
     public void render(PoseStack poseStack, int pMouseX, int pMouseY, float pPartialTick) {
+        if (!isVisible()) return;
+        RenderSystem.enableScissor(scissorX, scissorY, scissorWidth, scissorHeight);
         font.draw(poseStack, this.skillName.append(" " + skill.getLevel()), this.x, this.y, this.color);
 
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
@@ -63,6 +69,7 @@ public class SkillWidget implements GuiEventListener, Widget, NarratableEntry {
         GuiComponent.blit(poseStack, Math.round(this.x / 0.75F), this.y + this.font.lineHeight, 0F, 5F, width, 5, 182, 10);
         poseStack.scale(1F / 0.75F, 1F, 1F);
         RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
+        RenderSystem.disableScissor();
     }
 
     @Override
@@ -73,5 +80,12 @@ public class SkillWidget implements GuiEventListener, Widget, NarratableEntry {
     @Override
     public void updateNarration(NarrationElementOutput narrationElementOutput) {
         narrationElementOutput.add(NarratedElementType.POSITION, this.skillName);
+    }
+
+    public void setScissor(int x, int y, int width, int height) {
+        scissorX = x;
+        scissorY = y;
+        scissorWidth = width;
+        scissorHeight = height;
     }
 }
