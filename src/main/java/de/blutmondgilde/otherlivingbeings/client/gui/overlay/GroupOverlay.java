75f00f344af2ebf17e9dd3c4a1618baf69509ccf
Christian Raoulis <craoulis@googlemail.com>
2021-10-22 12:59:42 +0200

    add background to GroupOverlay

--- src/main/java/de/blutmondgilde/otherlivingbeings/client/gui/overlay/GroupOverlay.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/client/gui/overlay/GroupOverlay.java
@@ -5,8 +5,13 @@ import de.blutmondgilde.otherlivingbeings.client.ClientGroupHolder;
 import de.blutmondgilde.otherlivingbeings.client.gui.widget.GroupMemberWidget;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.client.gui.IIngameOverlay;
+import net.minecraftforge.fmlclient.gui.GuiUtils;
+
+import java.awt.*;
+import java.util.List;
 
 public class GroupOverlay implements IIngameOverlay {
+    private static final Color backgroundColor = new Color(19, 19, 19, 190);
 
     public GroupOverlay() {
 
@@ -15,18 +20,23 @@ public class GroupOverlay implements IIngameOverlay {
     @Override
     public void render(ForgeIngameGui gui, PoseStack mStack, float partialTicks, int width, int height) {
         if (!ClientGroupHolder.isInGroup()) return;
-        //TODO render background
 
-        int i = 0;
-        for (GroupMemberWidget widget : ClientGroupHolder.getMemberData()
+        List<GroupMemberWidget> widgets = ClientGroupHolder.getMemberData()
                 .stream()
                 .map(groupMemberData -> new GroupMemberWidget(groupMemberData.getDisplayName(), groupMemberData.getHealth(), groupMemberData.getMaxHealth()))
-                .toList()) {
+                .toList();
+        if (widgets.isEmpty()) return;
+        renderBackground(mStack, width, height, widgets.get(0).getWidth(), widgets.stream().map(GroupMemberWidget::getHeight).reduce(1, Integer::sum));
 
-            //TODO set position
+        int i = 0;
+        for (GroupMemberWidget widget : widgets) {
             widget.setX(width - widget.getWidth() - 2);
             widget.setY(height / 3 + widget.getHeight() * i++);
             widget.render(mStack, 0, 0, partialTicks);
         }
+    }
+
+    private void renderBackground(PoseStack stack, int screenWidth, int screenHeight, int width, int height) {
+        GuiUtils.drawGradientRect(stack.last().pose(), 0, screenWidth - width - 4, screenHeight / 3 - 4, screenWidth, screenHeight / 3 + height, backgroundColor.getRGB(), backgroundColor.getRGB());
     }
 }
