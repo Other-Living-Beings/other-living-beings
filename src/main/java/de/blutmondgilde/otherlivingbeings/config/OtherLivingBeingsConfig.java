c190014da7619e3a3ddc4c3712aa21ea9a637621
Christian Raoulis <craoulis@googlemail.com>
2021-10-24 10:38:52 +0200

    lower effect trigger chance ever 10 level from 5% to 0.5%

--- src/main/java/de/blutmondgilde/otherlivingbeings/config/OtherLivingBeingsConfig.java
+++ src/main/java/de/blutmondgilde/otherlivingbeings/config/OtherLivingBeingsConfig.java
@@ -21,27 +21,27 @@ public class OtherLivingBeingsConfig implements ConfigData {
 
         public static class Lumberjack {
             @ConfigEntry.Gui.Tooltip
-            public float breakSpeedPerLevel = 0.05F;
+            public float breakSpeedPerLevel = 0.005F;
             @ConfigEntry.Gui.Tooltip
-            public float unbreakingChance = 0.05F;
+            public float unbreakingChance = 0.005F;
         }
 
         public static class Miner {
             @ConfigEntry.Gui.Tooltip
-            public float breakSpeedPerLevel = 0.05F;
+            public float breakSpeedPerLevel = 0.005F;
             @ConfigEntry.Gui.Tooltip(count = 2)
-            public float unbreakingChance = 0.05F;
+            public float unbreakingChance = 0.005F;
             @ConfigEntry.Gui.Tooltip(count = 2)
-            public float doubleLootChance = 0.05F;
+            public float doubleLootChance = 0.005F;
             @ConfigEntry.Gui.Tooltip(count = 2)
-            public float tripleLootChance = 0.05F;
+            public float tripleLootChance = 0.005F;
         }
 
         public static class Farmer {
             @ConfigEntry.Gui.Tooltip(count = 2)
-            public float doubleLootChance = 0.05F;
+            public float doubleLootChance = 0.005F;
             @ConfigEntry.Gui.Tooltip(count = 2)
-            public float growthTickChance = 0.05F;
+            public float growthTickChance = 0.005F;
         }
     }
 
